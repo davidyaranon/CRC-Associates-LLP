82b0aa4e25d661e4bc29adf012b2dea8be4e93bc
DAVID YARANON <dyaranon126@gmail.com>
2024-03-06 14:30:49 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CRC_Associates_LLP',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
